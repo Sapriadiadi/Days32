e397431a1c6db46457d53ac6889f777017cab855
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-11-09 21:52:44 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package days32;
+
+public class Main {
+    
+    public static void main(String[] args) {
+            //Soal membuat looping mencetak nama sbyk 10x
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i+" sapriadi");
+            
+        }
+    }
+}
